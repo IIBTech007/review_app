9e825d794ed6119b64dabe2d1eb738b7e2e99942
IIB <iib.tech357@gmail.com>
2021-03-04 09:45:39 +0500

    Forgot Password Screen edited

--- lib/Screens/WelcomeScreen/ForgotPassword.dart
+++ lib/Screens/WelcomeScreen/ForgotPassword.dart
@@ -11,6 +11,7 @@ class ForgotPasswordScreen extends StatefulWidget {
 
 class _ForgotPasswordScreenState extends State< ForgotPasswordScreen> {
   bool isVisible= true;
+  bool isVisible2= true;
 
   @override
   Widget build(BuildContext context) {
@@ -58,7 +59,7 @@ class _ForgotPasswordScreenState extends State< ForgotPasswordScreen> {
                               color: color1,
                             child: Container(
                               width: 400,
-                              height: 210,
+                              height: 350,
                               decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(10),
                                   //color: color3,
@@ -118,6 +119,64 @@ class _ForgotPasswordScreenState extends State< ForgotPasswordScreen> {
                                       textInputAction: TextInputAction.next,
                                     ),
                                   ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextFormField(
+                                      //controller: password,
+                                      style: TextStyle(color: color4,fontWeight: FontWeight.bold),
+                                      obscureText: isVisible,
+                                      keyboardType: TextInputType.visiblePassword,
+                                      decoration: InputDecoration(
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(color: color3, width: 1.0)
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(color: color3, width: 1.0)
+                                        ),
+
+                                        labelText: "New Password",
+                                        labelStyle: TextStyle(color: color3, fontWeight: FontWeight.bold),
+                                        suffixIcon: IconButton(icon: Icon(isVisible?Icons.visibility:Icons.visibility_off,color: color3,size: 27),onPressed: () {
+                                          setState(() {
+                                            if(isVisible){
+                                              isVisible= false;
+                                            }else{
+                                              isVisible= true;
+                                            }
+                                          });
+                                        },),//Icon(Icons.https,color: yellowColor,size: 27,)
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: TextFormField(
+                                      //controller: password,
+                                      style: TextStyle(color: color4,fontWeight: FontWeight.bold),
+                                      obscureText: isVisible,
+                                      keyboardType: TextInputType.visiblePassword,
+                                      decoration: InputDecoration(
+                                        focusedBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(color: color3, width: 1.0)
+                                        ),
+                                        enabledBorder: OutlineInputBorder(
+                                            borderSide: BorderSide(color: color3, width: 1.0)
+                                        ),
+                                        labelText: "Confirm Password",
+                                        labelStyle: TextStyle(color: color3, fontWeight: FontWeight.bold),
+                                        suffixIcon: IconButton(icon: Icon(isVisible2?Icons.visibility:Icons.visibility_off,color: color3,size: 27),onPressed: () {
+                                          setState(() {
+                                            if(isVisible2){
+                                              isVisible2= false;
+                                            }else{
+                                              isVisible2= true;
+                                            }
+                                          });
+                                        },),//Icon(Icons.https,color: yellowColor,size: 27,)
+                                      ),
+
+                                    ),
+                                  ),
                                 ],
                               ),
                             ),
@@ -128,14 +187,14 @@ class _ForgotPasswordScreenState extends State< ForgotPasswordScreen> {
                     ],
                   ),
                   Positioned(
-                    top: 530,
+                    top: 675,
                     left: 30,
                     right: 30,
                     child: Padding(
                       padding: const EdgeInsets.all(5.0),
                       child: InkWell(
                         onTap: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=> NewPasswordScreen()));
+                          //Navigator.push(context, MaterialPageRoute(builder: (context)=> NewPasswordScreen()));
                         },
                         child: Center(
                           child: Card(
